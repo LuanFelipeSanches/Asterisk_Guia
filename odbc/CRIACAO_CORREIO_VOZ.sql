f22a9898fca56182c74476abaccad3ae4bd7307c
Luan Felipe Rodrigues Sanches <95869993+LuanFelipeSanches@users.noreply.github.com>
2022-09-22 22:55:33 -0300

    Dialplan basico e correio de voz

---
+++ odbc/CRIACAO_CORREIO_VOZ.sql
@@ -0,0 +1,7 @@
+INSERT INTO `asterisk`.`voicemail` (context,mailbox,password,fullname,email)
+VALUES
+('default','100','486541','Russell Bryant', 'russell@shifteight.org'),
+('default','101','957642','Leif Madsen', 'leif@shifteight.org'),
+('default','102','656844','Jared Smith', 'jared@shifteight.org'),
+('default','103','375416','Jim VanMeggelen', 'jim@shifteight.org')
+;
